a1a4c8fb1edaf76fe9668d8cb1dfab0b267c1945
Ariel Sardinha <odararepresentacoes@outlook.com.br>
2021-04-17 15:18:08 -0300

    adcionando itens na sacola

--- js.js
+++ js.js
@@ -4,6 +4,7 @@ const txt = document.getElementById('aside-sacola')
 const txt_checkbox = document.querySelectorAll('input.check')
 
 function min_max(){
+        
     if(document.getElementById('aside-min-max').value != " + "){
         aside_min_max.style.height = "53px"
         aside_min_max.style.width = "45px"
@@ -16,11 +17,16 @@ function min_max(){
         btn_min_max.style.left = "130px"
     }
 }
-for(let i = 0; i < txt_checkbox.length; i ++){
-    console.log(txt_checkbox[i])
-    if(txt_checkbox[i].checked == true){
-        console.log(txt_checkbox[i])
+function adcionar_carrinho(){
+    for(let i = 0; i < txt_checkbox.length; i ++){
+        if(txt_checkbox[i].checked == true){
+            txt.innerHTML += `<p>${txt_checkbox[i].value}</p>`
+        }
     }
+        aside_min_max.style.height = "300px"
+        aside_min_max.style.width = "300px"
+        btn_min_max.value = " - "
+        btn_min_max.style.left = "130px"
 }
 
 
