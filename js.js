4065f4a58b8488f79c282d7b92a6ad2eb93e0b24
Ariel Sardinha <odararepresentacoes@outlook.com.br>
2021-04-17 14:02:45 -0300

    tentendo corrigir bug

--- js.js
+++ js.js
@@ -1,18 +1,21 @@
 const aside_min_max =  document.querySelector('aside')
 const btn_min_max = document.getElementById("aside-min-max")
- 
+const txt = document.getElementById('aside-sacola')
+const txt_checkbox = document.querySelectorAll('input.check')
+
 function min_max(){
     if(document.getElementById('aside-min-max').value != " + "){
         aside_min_max.style.height = "53px"
         aside_min_max.style.width = "45px"
         btn_min_max.value = " + "
-        btn_min_max.style.left = "7px"
+        btn_min_max.style.left = "0px"
     }else{
         aside_min_max.style.height = "300px"
         aside_min_max.style.width = "300px"
         btn_min_max.value = " - "
-        btn_min_max.style.left = "265px"
+        btn_min_max.style.left = "130px"
     }
 }
+console.log(txt_checkbox[0].checked)
 
 
